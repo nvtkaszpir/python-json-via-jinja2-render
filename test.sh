5927f8cc1eb0981d0427c92fc6d30d2157b4a9ac
Michał Sochoń <kaszpir@gmail.com>
2021-02-05 23:47:25 +0100

    Add aut-generation of readme and tests

--- test.sh
+++ test.sh
@@ -2,69 +2,16 @@
 set -exuo pipefail
 # render some examples
 
-# AWS ECR scans
-python render.py \
-  -i examples/aws_ecr_scan_result.json \
-  -t templates/aws_ecr_scan_result_plaintext_short.j2 \
-  -o examples/aws_ecr_scan_result_plaintext_short.txt
+# eat your own dog food
+yq -M '.' examples/readme_table.yaml |
+  python render.py \
+    --template templates/readme.j2 \
+    --output README.md
 
-python render.py \
-  -i examples/aws_ecr_scan_result.json \
-  -t templates/aws_ecr_scan_result_plaintext_long.j2 \
-  -o examples/aws_ecr_scan_result_plaintext_long.txt
+yq -M '.' examples/readme_table.yaml |
+  python render.py \
+    --template templates/_generate_test_extra.j2 \
+    --output test_extra.sh
 
-python render.py \
-  -i examples/aws_ecr_scan_result.json \
-  -t templates/aws_ecr_scan_result_html.j2 \
-  -o examples/aws_ecr_scan_result_html.html
-
-python render.py \
-  -i examples/aws_ecr_scan_result.json \
-  -t templates/aws_ecr_scan_result_csv.j2 \
-  -o examples/aws_ecr_scan_result_csv.csv
-
-# render 'broken' files, such as unsupported images
-python render.py \
-  -i examples/aws_ecr_scan_result_failed.json \
-  -t templates/aws_ecr_scan_result_plaintext_short.j2 \
-  -o examples/aws_ecr_scan_result_plaintext_short_failed.txt
-
-python render.py \
-  -i examples/aws_ecr_scan_result_failed.json \
-  -t templates/aws_ecr_scan_result_plaintext_long.j2 \
-  -o examples/aws_ecr_scan_result_plaintext_long_failed.txt
-
-python render.py \
-  -i examples/aws_ecr_scan_result_failed.json \
-  -t templates/aws_ecr_scan_result_html.j2 \
-  -o examples/aws_ecr_scan_result_html_failed.html
-
-python render.py \
-  -i examples/aws_ecr_scan_result_failed.json \
-  -t templates/aws_ecr_scan_result_csv.j2 \
-  -o examples/aws_ecr_scan_result_failed_csv.csv
-
-# docker inspect
-python render.py \
-  -i examples/docker_inspect.json \
-  -t templates/docker_inspect_plaintext.j2 >examples/docker_inspect_plaintext.yaml
-
-# dive docker://quay.io/kaszpir/python-json-via-jinja2-render:latest --json /dev/stdout > examples/dive.json
-# redirect to stdout cause dive likes to append to existing file
-python render.py \
-  -i examples/dive.json \
-  -t templates/dive_plaintext.j2 >examples/dive_plaintext.txt
-
-python render.py \
-  -i examples/dive.json \
-  -t templates/dive_html.j2 >examples/dive_html.htnl
-
-# docker based runs
-
-docker run -v "$(pwd)/templates/:/app/templates:ro" -i quay.io/kaszpir/python-json-via-jinja2-render -t templates/aws_ecr_scan_result_html.j2 \
-  <examples/aws_ecr_scan_result.json \
-  >output.html
-
-docker run -v "$(pwd)/templates/:/app/templates:ro" -i quay.io/kaszpir/python-json-via-jinja2-render -t templates/aws_ecr_scan_result_html.j2 \
-  <examples/aws_ecr_scan_result_failed.json \
-  >output_failed.html
+chmod +x test_extra.sh
+./test_extra.sh
---
+++ test_extra.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+set -x
+set -e
+# auto-generated file to run rendering of the files
+# this file looks like a real nightmare under shellcheck :D
+# don't even try to run shfmt on jinja2 template.
+
+# [wagoodman/dive](https://github.com/wagoodman/dive) HTML
+# 
+
+python render.py \
+  --input    examples/dive.json \
+  --template templates/dive_html.j2 \
+  --output   examples/dive_html.html
+
+
+# [wagoodman/dive](https://github.com/wagoodman/dive) plain text
+# 
+
+python render.py \
+  --input    examples/dive.json \
+  --template templates/dive_plaintext.j2 \
+  --output   examples/dive_plaintext.txt
+
+
+# AWS ECR security scan CSV
+# 
+
+python render.py \
+  --input    examples/aws_ecr_scan_result.json \
+  --template templates/aws_ecr_scan_result_csv.j2 \
+  --output   examples/aws_ecr_scan_result_csv.csv
+
+
+# AWS ECR security scan HTML
+# 
+
+python render.py \
+  --input    examples/aws_ecr_scan_result.json \
+  --template templates/aws_ecr_scan_result_html.j2 \
+  --output   examples/aws_ecr_scan_result_html.html
+
+
+# AWS ECR security scan plaintext long
+# 
+
+python render.py \
+  --input    examples/aws_ecr_scan_result.json \
+  --template templates/aws_ecr_scan_result_plaintext_long.j2 \
+  --output   examples/aws_ecr_scan_result_plaintext_long.txt
+
+
+# AWS ECR security scan plaintext short
+# 
+
+python render.py \
+  --input    examples/aws_ecr_scan_result.json \
+  --template templates/aws_ecr_scan_result_plaintext_short.j2 \
+  --output   examples/aws_ecr_scan_result_plaintext_short.txt
+
+
+# docker inspect plain text to simulate YAML cause we can!
+# This one is rendered totally within docker container
+
+docker run \
+    -v "$(pwd)/templates/:/app/templates:ro" \
+    -i quay.io/kaszpir/python-json-via-jinja2-render \
+        -t templates/docker_inspect_plaintext.j2 \
+        <examples/docker_inspect.json \
+        >examples/docker_inspect_plaintext.yaml
